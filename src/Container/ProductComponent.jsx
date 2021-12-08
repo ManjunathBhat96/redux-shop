cdc7cc42d183a85a025377438d0bace376b8ffa4
ManjunathBhat96 <bhatManjunath96@gmail.com>
2021-12-08 23:44:15 +0530

    listing is added

--- src/Container/ProductComponent.jsx
+++ src/Container/ProductComponent.jsx
@@ -5,18 +5,20 @@ function ProductComponent(props) {
   // const products = useSelector(state => state.allProducts.products);
   // const {id, title} = products;
   return (
-    <>
-      <div className=" four column wide">
-        <div className="ui link cards">
-          <div className="card">
-            <div className="image"></div>
-            <div className="content">
-              <div className="header">{props.title}</div>
-            </div>
+    <div className="four column wide" style={{width: '100%'}}>
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image">
+            <img src={props.image} alt="" />
+          </div>
+          <div className="content">
+            <div className="header">{props.title}</div>
+            <div className="meta price">{props.price}</div>
+            <div className="meta">{props.category}</div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/Container/ProductListing.jsx
+++ src/Container/ProductListing.jsx
@@ -23,9 +23,20 @@ function ProductListing() {
   const products = useSelector(state => state.allProducts.products);
 
   return (
-    <div className="ui grid container">
-      {products.map(item => {
-        <ProductComponent title={item.title} />;
+    <div
+      className="ui grid container"
+      style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
+      {products?.map(item => {
+        return (
+          <ProductComponent
+            title={item.title}
+            price={item.price}
+            category={item.category}
+            image={item.image}
+          />
+        );
+
+        // <p>{item.title}</p>;
       })}
     </div>
   );

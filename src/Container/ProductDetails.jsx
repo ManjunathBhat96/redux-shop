f35ca84635ae24255698244ce65ef0311a545fe3
ManjunathBhat96 <bhatManjunath96@gmail.com>
2021-12-12 23:22:36 +0530

    details page added

--- src/Container/ProductDetails.jsx
+++ src/Container/ProductDetails.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState, useCallback} from 'react';
+import ProductComponent from './ProductComponent';
 
 function ProductDetails() {
+  const [productDetails, setProductDetails] = useState({});
+  const getProductDetails = useCallback(async () => {
+    try {
+      const result = await axios.get(' https://fakestoreapi.com/products/1');
+      setProductDetails(result.data);
+    } catch (error) {
+      console.log('Error', error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+
   return (
-    <div className=" ui fixed menu">
-      <div className=" ui container center">
+    <div class="hidden divider section ui four column doubling flex container">
+      {/* <div className=" ui fixed menu"> */}
+      <div class="column">
         <h2>Product Details</h2>
       </div>
+
+      <div className="column">
+        <ProductComponent
+          title={productDetails.title}
+          price={productDetails.price}
+          category={productDetails.category}
+          image={productDetails.image}
+          id={productDetails.id}
+        />
+      </div>
+      {/* </div> */}
     </div>
   );
 }
